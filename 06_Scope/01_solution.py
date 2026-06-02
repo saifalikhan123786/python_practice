62b8b45fbe45cf927f4aa7fecf32ae1252301859
Saif Ali Khan <saifalikhan123@example.com>
2026-06-02 17:04:24 +0500

    clousar problem solved

---
+++ 06_Scope/01_solution.py
@@ -0,0 +1,22 @@
+username= "chaiaurcode"
+
+def func():
+    # username= "chai" 
+    print(username)
+
+print(username)
+func()   
+
+
+
+x=99
+
+def f1():
+    x=88
+    def f2():
+      print(x)
+    return f2
+myResult=f1()
+myResult()
+
+
